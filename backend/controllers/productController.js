e284314e2a621f92a94ed821876e24736b64e31a
Bhavy Bansal <bhavy.209309023@muj.manipal.edu>
2022-06-22 02:46:58 +0530

    added Search Filter & Pagination functions to backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,24 +1,28 @@
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
-    
     const product = await Product.create(req.body);
 
     res.status(201).json({
         success: true,
         product
     });
-
 });
-
 
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-    
-    const products = await Product.find();
+    const resultPerPage = 8;
+    const productCount = await Product.countDocuments();
+
+    const apiFeature = new ApiFeatures(Product.find(), req.query)
+        .search()
+        .filter()
+        .pagination(resultPerPage);
+    const products = await apiFeature.query;
 
     res.status(200).json({
         success: true,
@@ -79,7 +83,7 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        product
+        product,
+        productCount,
     });
-
 });
